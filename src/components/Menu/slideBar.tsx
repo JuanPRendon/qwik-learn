cb429df7b3a0b55e865485dd8649698beb43bd55
Juan Pablo Atehortua Rendon <TIC8@auralac.local>
2023-03-13 17:20:35 -0500

    hola juan del futuro

---
+++ src/components/Menu/slideBar.tsx
@@ -0,0 +1,14 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(()=>{
+    return(
+        <div class='slidebar'>
+         <ul>
+            <li>dashboard</li>
+            <li>conteo</li>
+            <li>usuarios</li>
+            <li>materiales</li>
+        </ul>
+        </div>
+    )
+})
